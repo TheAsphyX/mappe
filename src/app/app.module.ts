b55d476c823b3f21eae721e189b06e771d6ed444
Luigi Frasca <luigi.frasca@ap.dipvvf.it>
2017-04-13 09:34:14 +0200

    primo commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule, FormControl } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
+import { App } from './app.component';
+import { AgmCoreModule } from "@agm/core";
 
 @NgModule({
   declarations: [
-    AppComponent
+    App
   ],
   imports: [
+    AgmCoreModule.forRoot({
+      apiKey: "AIzaSyCUyaLim6v4CB_eo-oJmeDlvPCQxwHha70",
+      libraries: ["places"]
+    }),
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [App]
 })
 export class AppModule { }
